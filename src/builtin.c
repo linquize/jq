d34d75715afff17daee578fccee841d223c68b67
Linquize <linquize@yahoo.com.hk>
2020-12-14 21:54:09 +0800

    my_timegm: Fix build on Windows

--- src/builtin.c
+++ src/builtin.c
@@ -1204,6 +1204,10 @@ static jv tm2jv(struct tm *tm) {
 static time_t my_timegm(struct tm *tm) {
 #ifdef HAVE_TIMEGM
   return timegm(tm);
+#elif defined(WIN32) /* HAVE_TIMEGM */
+    struct tm tm1;
+    memcpy(&tm1, tm, sizeof(struct tm));
+    return _mkgmtime(&tm1);
 #else /* HAVE_TIMEGM */
   char *tz;
 
